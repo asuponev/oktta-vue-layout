ac9bc777fdd5327d1aa7add4c39295c9c5fd1c20
Andrew <andrew.suponev@gmail.com>
2023-05-15 18:14:55 +0400

    fix: hover effect

--- src/components/ClientsList.vue
+++ src/components/ClientsList.vue
@@ -13,35 +13,86 @@ export default {
         { id: 8, name: 'Гейзер' },
         { id: 9, name: 'Espadro' },
         { id: 10, name: 'Kidsee' },
-      ]
+      ],
+      positionYImg: -13
     }
-  }
+  },
+  methods: {
+    onListHover(clientId) {
+      this.getPositionImg()
+      document.querySelector('.list-visual__inner').classList.add('active')
+      document.querySelectorAll('.list-visual__item').forEach(cover => {
+        if (clientId == cover.dataset.client) {
+          cover.classList.add('active')
+        }
+      })
+    },
+    onItemLeave(clientId) {
+      document.querySelectorAll('.list-visual__item').forEach(cover => {
+        if (clientId == cover.dataset.client) {
+          cover.classList.remove('active')
+        }
+      })
+    },
+    onListLeave() {
+      document.querySelector('.list-visual__inner').classList.remove('active')
+      document.querySelectorAll('.list-visual__item').forEach(cover => cover.classList.remove('active'))
+    },
+    getPositionImg() {
+      const clientsWrapper = document.querySelector('.clients-list')
+      if (clientsWrapper.getBoundingClientRect().top > window.innerHeight * 0.3) {
+        this.positionYImg = -13
+      } else if (clientsWrapper.getBoundingClientRect().bottom < window.innerHeight * 0.7) {
+        this.positionYImg = 113
+      } else {
+        this.positionYImg = 50
+      }
+    }
+  },
 }
 </script>
 
 <template>
-  <ul class="list">
-    <li
-      v-for="client in clients"
-      :key="client.id"
-      class="list__item"
-    >
-      <div class="list__item-wrapper">
-        <h3 class="list__item-title">{{ client.name }}</h3>
-        <button class="list__item-btn">
-          <svg>
-            <use xlink:href="@/assets/icons/_sprite.svg#arrow-link">
-            </use>
-          </svg>
-        </button>
-      </div>
-      <div class="list__item-cover">
-        <div>
-          <p>{{ client.name }}</p>
+  <div
+    class="clients-list"
+    @mouseleave="() => onListLeave()"
+  >
+    <ul class="list">
+      <li
+        v-for="client in clients"
+        :key="client.id"
+        @mouseover="() => onListHover(client.id)"
+        @mouseleave="() => onItemLeave(client.id)"
+        class="list__item"
+      >
+        <div class="list__item-wrapper">
+          <h3 class="list__item-title">{{ client.name }}</h3>
+          <button class="list__item-btn">
+            <svg>
+              <use xlink:href="@/assets/icons/_sprite.svg#arrow-link" />
+            </svg>
+          </button>
+        </div>
+      </li>
+    </ul>
+    <div class="list-visual">
+      <div
+        class="list-visual__inner"
+        :style="{ 'transform': `translate3d(146%, ${positionYImg}%, 0)` }"
+      >
+        <div
+          v-for="client in clients"
+          :key="client.id"
+          :data-client="client.id"
+          class="list-visual__item"
+        >
+          <div>
+            <p>{{ client.name }}</p>
+          </div>
         </div>
       </div>
-    </li>
-  </ul>
+    </div>
+  </div>
 </template>
 
 <style lang="scss" scoped>
@@ -99,8 +150,10 @@ export default {
     &-cover {
       display: none;
       position: absolute;
-      top: -48px;
+      top: 0;
+      bottom: 0;
       left: calc(25% + 20px);
+      margin: auto;
       height: 360px;
       width: 260px;
       background: $color-orange;
@@ -116,39 +169,73 @@ export default {
         transition: width 0.3s ease-in-out, opacity 0.3s ease-in-out;
       }
     }
-  }
-}
 
-@media (min-width: 769px) {
-  .list__item:hover {
-    background: $color-general-green;
+    &:hover {
+      background: $color-general-green;
 
-    .list__item-btn {
-      border: 1px solid $color-general-dark;
-      background: $color-general-dark;
+      .list__item-btn {
+        border: 1px solid $color-general-dark;
+        background: $color-general-dark;
 
-      svg {
-        stroke: $color-general-white;
-      }
+        svg {
+          stroke: $color-general-white;
+        }
 
-      &:active {
-        background: $color-hover-bg;
-        border: 1px solid $color-hover-bg;
-      }
-    }
-
-    .list__item-cover {
-      display: block;
-
-      div {
-        opacity: 1;
-        width: 100%;
+        &:active {
+          background: $color-hover-bg;
+          border: 1px solid $color-hover-bg;
+        }
       }
     }
   }
 }
 
-@media (max-width: 900px) {
+.clients-list {
+  position: relative;
+}
+
+.list-visual {
+  pointer-events: none;
+  position: absolute;
+  top: 0;
+  left: 0;
+
+  &__inner {
+    border-radius: 21px;
+    overflow: hidden;
+    position: relative;
+    background: $color-orange;
+    width: 260px;
+    height: 360px;
+    opacity: 0;
+    transition: opacity 0.3s ease-in-out, transform 0.3s ease-in-out;
+
+    &.active {
+      opacity: 1;
+    }
+  }
+
+  &__item {
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    top: 0;
+    background: $color-orange;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    opacity: 0;
+    transform: translate3d(-100%, 0, 0);
+    transition: opacity 0.1s ease-in-out, transform 0.1s ease-in-out;
+
+    &.active {
+      transform: translate3d(0, 0, 0);
+      opacity: 1;
+    }
+  }
+}
+
+@media (max-width: 1024px) {
   .list {
     &__item {
 
